5519c19e49fcff1e7ca0ffe25248784e3f7397d5
John Southerland <jsoutherland@me.com>
2023-04-13 14:15:19 -0600

    first stab at a QueueProcessor Interceptor

---
+++ src/main/java/com/singularity/com/cisco/josouthe/QueueProcessorInterceptor.java
@@ -0,0 +1,48 @@
+package com.singularity.com.cisco.josouthe;
+
+import com.appdynamics.agent.api.Transaction;
+import com.appdynamics.instrumentation.sdk.Rule;
+import com.appdynamics.instrumentation.sdk.SDKClassMatchType;
+import com.appdynamics.instrumentation.sdk.template.AGenericInterceptor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//com.pega.pegarules.session.internal.async.agent.QueueProcessor:execute:620
+public class QueueProcessorInterceptor extends AGenericInterceptor {
+
+    @Override
+    public List<Rule> initializeRules() {
+        List<Rule> rules = new ArrayList<Rule>();
+        rules.add(new Rule.Builder("com.pega.pegarules.session.internal.async.agent.QueueProcessor")
+                .classMatchType(SDKClassMatchType.MATCHES_CLASS)
+                .methodMatchString("execute") //the beginning of a BT
+                .build());
+        return rules;
+    }
+
+    public Object onMethodBegin(Object object, String className, String methodName, Object[] param) {
+        getLogger().info(String.format("onMethodBegin %s.%s( %s )", className, methodName, paramsToString(param)));
+
+        return null;
+    }
+
+    private Object paramsToString(Object[] param) {
+        if( param == null || param.length == 0 ) return "";
+        StringBuilder sb = new StringBuilder();
+        for( int i =0 ; i< param.length; i++ ) {
+            sb.append( param[i].getClass().getCanonicalName() );
+            sb.append(" ").append(String.valueOf(param[i]));
+            if( i < param.length ) sb.append(", ");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public void onMethodEnd(Object state, Object object, String className, String methodName, Object[] params, Throwable exception, Object returnVal) {
+        if( state == null ) return;
+        Transaction transaction = (Transaction) state;
+        if( exception != null ) transaction.markAsError( exception.getMessage() );
+        transaction.end();
+    }
+}
